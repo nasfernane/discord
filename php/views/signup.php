89466f5e774b6fa6e9d0e0be6f900107586a7a6b
nasfernane <nasfernane@gmail.com>
2021-03-10 14:23:33 +0100

    maj signup

--- php/views/signup.php
+++ php/views/signup.php
@@ -1,1 +1,21 @@
-<h1>blabla</h1>
+<div class="logBackground"></div>
+
+<div class="signupContainer">
+    <h1>Créer un compte</h1>
+    <form class="signupContainer__form" method="POST" action="createUser.php">
+        <div class="signupContainer__form__field">
+            <label for="signUpEmail">e-mail</label>
+            <input type="text" id="signUpEmail" />
+        </div>
+        <div class="signupContainer__form__field">
+            <label for="signUpUser">Nom d'utilisateur</label>    
+            <input type="text" id="signUpUser" />
+        </div>
+        <div class="signupContainer__form__field">
+            <label for="signUpPw">Mot de passe</label>
+            <input type="password" id="signUpPw">
+        </div>
+        <button class="signupContainer__form--btn" type="submit">Continuer</button>
+        <a href="?page=login">Tu as déjà un compte ?</a>
+    </form>
+</div>
